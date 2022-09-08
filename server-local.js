0ebecefac4b38c9407dc176e29da39a3aef9b193
Kevin Kiil <kekiil@ttu.ee>
2022-09-08 11:27:01 +0300

    server.js change

--- server-local.js
+++ server-local.js
@@ -1,5 +1,12 @@
-'use strict';
+// 'use strict';
 
-const app = require('./express/server');
+// const app = require('./express/server');
+const express = require("express");
+const app = express();
+
+app.get('/', function(req, res) {
+    const name = req.query.name;
+    res.send('Tere, ' + name + '!')
+  })
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
