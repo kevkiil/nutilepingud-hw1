4bf0ecdf178e720ece83095dc9ac8cf6c37d92b8
Kevin Kiil <kekiil@ttu.ee>
2022-09-08 11:54:31 +0300

    first commit

--- express/server.js
+++ express/server.js
@@ -1,51 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const router = express.Router();
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-    const name = req.query.name;
-    res.send('Tere, ' + name + '!')
-  })
+const router = express.Router();
+router.get('/', (req, res) => {
+  const name = req.query.name;
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<h1>Tere, ${name}!</h1>`);
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-// 'use strict';
-// const express = require('express');
-// const path = require('path');
-// const serverless = require('serverless-http');
-// const app = express();
-// const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-// const router = express.Router();
-// app.use('/', function(req, res) {
-//   const name = req.query.name;
-//   res.send('Tere, ' + name + '!')
-  // if(name === undefined) {
-  //   res.send('Add your name to the end of the URL like --> https://kevkiil.netlify.app/?name=YourName')
-  // }
-  // else{
-  // res.send('Tere, ' + name + '!')
-  // }
-// })
-
-
-// get('/', function(req, res) {
-//   const name = req.query.name;
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   if(name === undefined) {
-//     res.write(`<h1>Add your name to the end of the URL like --> https://kevkiil.netlify.app/?name=YourName</h1>`);
-//   } else {
-//     res.write(`<h1>Tere, ${name}!</h1>`);
-//   }
-  
-//   res.end();
-// });
-
-// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-// app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
-// module.exports = app;
-// module.exports.handler = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app);
--- server-local.js
+++ server-local.js
@@ -1,12 +1,5 @@
-// 'use strict';
+'use strict';
 
-// const app = require('./express/server');
-const express = require("express");
-const app = express();
-
-app.get('/', function(req, res) {
-    const name = req.query.name;
-    res.send('Tere, ' + name + '!')
-  })
+const app = require('./express/server');
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
