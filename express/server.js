bb3c9548ee9e6a4d67619aabdcad3516d7270f3c
Kevin Kiil <kekiil@ttu.ee>
2022-09-08 14:50:23 +0300

    commit

--- express/server.js
+++ express/server.js
@@ -1,15 +1,17 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
   const name = req.query.name;
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Tere, ${name}!</h1>`);
+  if(name === undefined) {
+      res.write(`<h1>Add your name to the end of the URL like --> https://kevkiil.netlify.app/?name=YourName</h1>`);
+    } else {
+      res.write(`<h1>Tere, ${name}!</h1>`);
+    }
   res.end();
 });
 
