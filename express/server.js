803fe4495d91e1538b5221ea611bdd13d94231cc
Kevin Kiil <kekiil@ttu.ee>
2022-09-08 16:42:26 +0300

    commit

--- express/server.js
+++ express/server.js
@@ -7,21 +7,20 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const name = req.query.name;
   res.writeHead(200, { 
-    'Content-Type': 'text/html', 'charset':'UTF-16' 
+    'Content-Type': 'text/html; charset=utf-16'
   });
   if(name === undefined) {
-      res.write(`<h1>Replace YourName in the end of the following link and see what happens --> https://kevkiil.netlify.app/?name=YourName</h1>`);
-      res.send('Eino, päisel pööripäeval läks õnneratas läbi nööride.');
+      res.write(`<h1>Replace YourName in the end of the following link and see what happens --> https://kevkiil.netlify.app/?name=YourName</h1>`, "utf-16");
     } else if (name === 'YourName')
     {
-      res.write(`<h1>Is your name actually YourName? Please change it and use your actual name ;)</h1>`);
+      res.write(`<h1>Is your name actually YourName? Please change it and use your actual name ;)</h1>`, "utf-16");
     }
     else if (name.toLowerCase().search('your') !== -1)
     {
-      res.write(`<h1>I see what you did there ;)</h1>`);
+      res.write(`<h1>I see what you did there ;)</h1>`, "utf-16");
     }
     else {
-      res.write(`<h1>Tere, ${name}!</h1>`);
+      res.write(`<h1>Tere, ${name}!</h1>`, "utf-16");
     }
   res.end();
 });
